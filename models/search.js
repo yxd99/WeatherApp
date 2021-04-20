e1a6f856be79ddcd7f0acd15e6c267d19d321b4c
Yexid Hernandez <yexid.9901@gmail.com>
2021-04-19 20:42:10 -0500

    Optimization in code.

--- models/search.js
+++ models/search.js
@@ -7,9 +7,22 @@ class Search {
         //TODO constructor
     }
 
+    get paramsMapBox(){
+        return {
+            'access_token': 'pk.eyJ1IjoieXhkOTkiLCJhIjoiY2tucGM3ZXRyMWVtYTJ3bzU1Z3dwZmhzNyJ9.iRaiBOX79qFCytBmj-7lmA',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async city( place = '' ) {
         try{
-            const result = await axios.get('https://reqres.in/api/users?page=2');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+                params: this.paramsMapBox
+            })
+            const result = await instance.get();
+
             console.log(result.data);
 
             return [];
